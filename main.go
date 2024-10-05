4ba48e3fd66c3ab99963fc2cf0bc394d71780798
pawan-sharma3110 <pawansharma93520@gmail.com>
2024-10-06 00:17:33 +0530

    welcome notes

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+	const cofrenceTickets = 50
+	var remainingTickets = 50
+	fmt.Printf("Welcome to %s booking application \n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v remaining ticketes \n", cofrenceTickets, remainingTickets)
+	fmt.Println("Get your tickets herre to attend")
+	var userNAme string
+	fmt.Println(userNAme)
+}
